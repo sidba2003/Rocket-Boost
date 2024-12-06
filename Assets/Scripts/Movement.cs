1e0605ea3d81b575e7ba827f2ecc104c14587322
sidme <bahlsiddharth123@gmail.com>
2024-12-06 06:59:02 +0000

    adding new level

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
@@ -21,6 +23,48 @@ public class Movement : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+        switch (other.gameObject.tag){
+            case "Fuel":
+                other.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    void OnCollisionEnter(Collision other){
+        switch (other.gameObject.tag){
+            case "Start": 
+                Debug.Log("Currently at start"); 
+                break;
+            case "Fuel": // this will most probably never be executed
+                other.gameObject.SetActive(false);
+                Debug.Log("Collected Fuel");
+                break;
+            case "Finish":
+                Debug.Log("You have finished this level!!!");
+                NextLevel();
+                break;
+            default:
+                RepeatLevel();
+                break;
+        }
+    }
+
+    void NextLevel()
+    {
+        int level = SceneManager.GetActiveScene().buildIndex;
+        level += 1;
+
+        SceneManager.LoadScene(level);
+    }
+
+    void RepeatLevel()
+    {
+        int level = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(level);
     }
 
     // Update is called once per frame
