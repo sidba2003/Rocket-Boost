1f05cecd8d9a550ff962d677ad5ffe20a970bf9b
sidme <bahlsiddharth123@gmail.com>
2024-12-18 18:28:04 +0000

    making a WebGL build of the game

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Update()
+    {   
+        if (Keyboard.current.qKey.wasPressedThisFrame){
+            Application.Quit();
+        }
+    }
+}
